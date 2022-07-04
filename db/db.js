47bd49c36a3a084160ad61150cfa9875c75b9036
M-J-B <barreiromartinj@gmail.com>
2022-07-04 18:35:27 -0300

    prueba

---
+++ db/db.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes, Op } = require('sequelize');
+
+const sequelize = new Sequelize({
+    database: "d1olon79irc05k",
+    username: "xhnhxkqzilphew",
+    password: "cfa5bb8911032d2c4f4061fe5f806edb9d315112f34cb40fc472b859bdc102ad",
+    host: "ec2-52-86-115-245.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
+    logging: false ,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      },
+      logging: false,
+      native: false,
+    },
+  },);
+/////////////////////////////////////// si ponemos el logging en false no me muestra todo el sql de sequelize
+module.exports = { sequelize, DataTypes, Op }
+sequelize.authenticate()
+    .then(() => console.log('conectados, postgress DB ok'))
+    .catch(err => console.log('algo fallo postgress DB' + err))  //para ver si esta conetado sequelize con db
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const cors = require("cors")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+// const User = require('./models/user')
+//////////DB///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const { sequelize } = require('./db/db')
+////////SERVIDOR////////
+let port = process.env.PORT || 8280
+app.listen(port, () => {
+    console.log('Server run on Port =>  ' + port)
+    sequelize.sync({ alter: true })
+})
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// //////USER////////////
+app.get('/show', async (req, res) => {
+       res.json({ "done": true })
+})
