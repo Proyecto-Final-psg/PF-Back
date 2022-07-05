d5b3c54aadb883607e4044d88c640f7804140a35
martin <barreiromartinj@gmail.com>
2022-07-05 20:48:38 -0300

    root create user inprogrees

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const cors = require("cors")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-// const User = require('./models/user')
+const User = require('./models/user')
+
+
 //////////DB///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const { sequelize } = require('./db/db')
 ////////SERVIDOR////////
-let port = process.env.PORT || 8280
+
+let port = process.env.PORT || 8180
 app.listen(port, () => {
     console.log('Server run on Port =>  ' + port)
     sequelize.sync({ alter: true })
@@ -16,5 +19,16 @@ app.listen(port, () => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // //////USER////////////
 app.get('/show', async (req, res) => {
-       res.json({ "done": "este cambio lo hizo Ramiro "})
+    res.json({ "done": "este cambio lo hizo Ramiro " })
+})
+
+
+
+/////create user/////
+
+app.post('/createUser', async (req, res) => {
+
+    let { name } = req.body
+
+    res.json({ "done": name })
 })
