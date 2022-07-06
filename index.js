a8e9cad05ae86888c552ad3dc27cf1cd9eea5067
Juan Manuel Cataldo <juanocataldo@gmail.com>
2022-07-05 22:05:16 -0300

    fixed model exports and routes. Also added /createUser

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const cors = require("cors")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-// const User = require('./models/user')
+const User = require('./models/Users.js')
 //////////DB///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const { sequelize } = require('./db/db')
+const { sequelize } = require('./db/db.js')
+
 ////////SERVIDOR////////
 let port = process.env.PORT || 8280
 app.listen(port, () => {
@@ -15,7 +16,21 @@ app.listen(port, () => {
 })
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // //////USER////////////
-app.get('/show', async (req, res) => {
-
-       res.json({ "done": "soy rama martin "})
+app.get('/getAllUsers', async (req, res) => {
+        const user = await User.findAll()
+       res.json({ "no muestro nada ": user})
 })
+
+app.post('/createUser', async(req, res) => {
+    const user = await User.create({
+        user_name:"test",
+        user_lastname:"test",
+        user_username:"test",
+        user_password:"test",
+        user_email:"test@jaja.com",
+        user_phone:"test",
+    })
+
+    res.json({created: user})
+})
+
