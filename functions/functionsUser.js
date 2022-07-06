054ec46e7dcc3260dda8455b11ca3579d575b204
Florencia Taburelli <flor.t.c@gmail.com>
2022-07-06 18:32:18 -0400

    elimine funciones de user sin uso

--- functions/functionsUser.js
+++ functions/functionsUser.js
@@ -15,32 +15,9 @@ module.exports = {
     },
     findAllUsers: async () => {
         return await User.findAll()
-    },
-    ///
-    findOrCreate: async (name, password) => {
-        return await User.findOrCreate({
-            where: { name },
-            defaults: {
-                name: name,
-                password: password
-            }
-        });
-
-    },
-    deleteUser: async (name) => {
-        return await User.destroy({
-            where: {
-                name: name
-            }
-        })
     }
    
 }
 
-// User.bulkCreate(req.body)  =====> se usa bulkCreate cuando queremos mandar un elemento
-// ///////////////////////////muy grande de una   seria un arreglo de objetos.
-//  User.create(name,password)
-//  User.destoy(name,password)
-// metodo tojSON()  muestra solo el objeto q se creo para hacer consol.log de las respuestas.
 
 
