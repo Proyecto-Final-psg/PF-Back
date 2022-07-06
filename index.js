a84d39cc0aa57d2bf88ba5dcb9ecd219f3e2f31c
Florencia Taburelli <flor.t.c@gmail.com>
2022-07-06 12:44:05 -0400

    deleted files useless

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 const User = require('./models/Users')
-const { getProducts, createProduct, getProductById, deleteProduct } = require('./functions/products')
+const { getProducts, createProduct, getProductById, deleteProduct } = require('./functions/functionsProduct')
 //////////DB///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const { sequelize } = require('./db/db')
 
