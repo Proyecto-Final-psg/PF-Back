991afdcbfd984e62c8664fe50f4a3fd25e0cdb8e
Florencia Taburelli <flor.t.c@gmial.com>
2022-07-05 22:09:46 -0400

    create model product, create get/products, create post/products

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 const User = require('./models/Users')
+const { getAllProducts, createProduct } = require('./functions/products')
 //////////DB///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const { sequelize } = require('./db/db')
 
@@ -38,11 +39,11 @@ app.post('/createUser', async(req, res) => {
 
 
 
-/// PRODUCTS //////
+/// PRODUCTS /////
 
 app.get('/products', async (req, res) => {
     try {
-        res.json(getAllProducts()) 
+        res.json(await getAllProducts()) 
     } catch (error) {
         res.status(401).json(error.message)
     }
@@ -51,7 +52,7 @@ app.get('/products', async (req, res) => {
 app.post('/products', async (req, res) => {
     try {
         let {name, stock, price, img, type, description, thc, cbd, cannabis, hashOil} = req.body
-        res.json(createProduct(name, stock, price, img, type, description, thc, cbd, cannabis, hashOil)) 
+        res.json(await createProduct(name, stock, price, img, type, description, thc, cbd, cannabis, hashOil)) 
     } catch (error) {
         res.status(401).json(error.message)
     }
--- models/Product.js
+++ models/Product.js
@@ -1,35 +1,23 @@
 const { sequelize, DataTypes, Op } = require("../db/db") //siempre importo el mismo sequelize ya creado en db .
 
 const Product = sequelize.define('product', {
-    // Model attributes are defined here
-    id:{
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false,
-        unique: true
-      },
       name: {
         type: DataTypes.STRING,
       
       },
-      // name, stock, price, img, type, description, thc, cbd, cannabis, hashOil
       stock: {
         type: DataTypes.INTEGER,
         defaultValue: 100
       },
       price: {
         type: DataTypes.INTEGER,
-        // precio random default
       },
       img: {
         type: DataTypes.STRING,
-        // URL for the main photo of the product.
       },
       type: {
         type: DataTypes.STRING,
       },
-      // Type of product.
       description: {
         type: DataTypes.TEXT
       },
@@ -45,8 +33,9 @@ const Product = sequelize.define('product', {
       hashOil: {
           type: DataTypes.FLOAT
       },
-    }
-    
+    },{
+        timestamps: true
+    }   
 );
 
 
