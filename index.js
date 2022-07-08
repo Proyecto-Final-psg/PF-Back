e1236b5ff452038c348f9e221bcdbe2304548a38
martin <barreiromartinj@gmail.com>
2022-07-07 21:55:18 -0300

    genial

--- index.js
+++ index.js
@@ -132,9 +132,18 @@ app.get('/productos', async (req, res) => {
 })
 //////Crear User/////////
 app.post('/ath0log', async (req, res) => {
-    let { email, name } = req.body
-    let user_email = email
-    let user_name = name
-    let creado = await findOrCreate(user_email, user_name)
-    res.json(creado)
+    try {
+        let { email, name } = req.body
+        let user_email = email
+        let user_name = name
+        let creado = await findOrCreate(user_email, user_name)
+        res.json(creado)
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({
+            name: error.name,
+            msg: error.message
+        })
+    }
+
 })
