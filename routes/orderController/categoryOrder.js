dc552de5660a7881d11f184a5b437a0a6f0890ba
Ramiro <ramirogrisales@gmail.com>
2022-07-08 16:36:09 -0500

    odenamiento por cbd

--- routes/orderController/categoryOrder.js
+++ routes/orderController/categoryOrder.js
@@ -2,8 +2,8 @@ const {getProducts} = require('../controller/functionsProduct')
 
 const orderCbd = async (array, setOrder) =>{
     return array.sort(function(a,b){
-        const numA = parseInt(a.id)
-        const numB = parseInt(b.id)
+        const numA = parseInt(a.cbd)
+        const numB = parseInt(b.cbd)
         if(setOrder === true){
             if(numA<numB) return -1
         } 
