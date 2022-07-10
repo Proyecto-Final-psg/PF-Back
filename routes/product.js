cd4721ec352e1aa28f43a4d8c239af01ad4a3e28
martin <barreiromartinj@gmail.com>
2022-07-10 16:50:04 -0300

    algoritmo carga automatica productos

--- routes/product.js
+++ routes/product.js
@@ -1,28 +1,39 @@
-const {Router} = require('express')
+const { Router } = require('express')
 const { getProducts, createProduct, getProductById, deleteProduct, getProductByName, uploadProducts, uploadCategories, updateProduct } = require('../controllers/products/functionsProduct')
 const { getCategories } = require('../controllers/categories/functionCategory')
-const {orderCbd, orderThc, orderPrice} = require('../controllers/products/orders')
+const { orderCbd, orderThc, orderPrice } = require('../controllers/products/orders')
 const { filterByCategory } = require('../controllers/products/orders')
 const { set } = require('lodash')
 const router = Router();
 
 ////  --   RUTA PARA CARGAR CATEGORIAS Y PRODUCTOS A LA BD ----
-router.get('/uploadDb', async (req, res) => {   
+const chargeDb = async () => {
+    let busqueda = await getCategories()
+    if (busqueda == 0) {
+        let categoriesUpload = await uploadCategories()
+        if (categoriesUpload > 0) {
+            uploadProducts()
+            console.log('se cargaron todos los productos')
+        } else {
+            console.log('No se cargaron los productos')
+        }
+    }
+}
+chargeDb()
+router.get('/uploadDb', async (req, res) => {
     try {
-       let categoriesUpload = await uploadCategories()
-      
-        if(categoriesUpload > 0){
+        let categoriesUpload = await uploadCategories()
+        if (categoriesUpload > 0) {
             uploadProducts()
             res.json('se cargaron')
-        }else{
-           res.json('No se cargaron') 
+        } else {
+            res.json('No se cargaron')
         }
-        
+
     } catch (error) {
         res.status(401).json(error.message)
     }
 })
- 
 ///////////////////////////////////////////////////////////////
 
 router.get('/', async (req, res) => {
@@ -60,30 +71,30 @@ router.put('/:id', async (req, res) => {
 })
 
 router.get('/orderCbd/:setOrder', async (req, res) => {
-    let { setOrder} = req.params
+    let { setOrder } = req.params
     try {
         res.json(await orderCbd(await getProducts(), setOrder))
-        
+
     } catch (error) {
         res.status(401).json(error.message)
     }
 })
 
 router.get('/orderThc/:setOrder', async (req, res) => {
-    let { setOrder} = req.params
+    let { setOrder } = req.params
     try {
         res.json(await orderThc(await getProducts(), setOrder))
-        
+
     } catch (error) {
         res.status(401).json(error.message)
     }
 })
 
 router.get('/orderPrice/:setOrder', async (req, res) => {
-    let { setOrder} = req.params
+    let { setOrder } = req.params
     try {
         res.json(await orderPrice(await getProducts(), setOrder))
-        
+
     } catch (error) {
         res.status(401).json(error.message)
     }
@@ -107,8 +118,8 @@ router.delete('/:id', async (req, res) => {
 
 router.get('/filter/:category', async (req, res) => {
     try {
-        const {category} = req.params
-        res.json(await filterByCategory(await getProducts() ,category))
+        const { category } = req.params
+        res.json(await filterByCategory(await getProducts(), category))
     } catch (error) {
         res.status(401).json(error.message)
     }
