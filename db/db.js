aab1aedc5c47624e39db4ee7e1d3464c6879d375
martin <barreiromartinj@gmail.com>
2022-07-13 19:13:51 -0300

    order

--- db/db.js
+++ db/db.js
@@ -17,8 +17,27 @@ const { Sequelize, DataTypes, Op } = require('sequelize');
 //     },
 //   },);
 
-  const sequelize = new Sequelize('postgres://postgres:cr1st1n4buf1d1s@localhost:5432/martinPrueba', { logging: false })
+///////////desarrollo////////
 
+const sequelize = new Sequelize({
+  database: "dpbsa0u9meu46",
+  username: "macdjezdluzphj",
+  password: "dde0dafe5e2d7c95daa72fd7b0ed4c8ca67e4ded754ff663d8672f0f5f94f0d4",
+  host: "ec2-3-223-169-166.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  logging: false ,
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    },
+    logging: false,
+    native: false,
+  },
+},);
+
+  
 /////////////////////////////////////// si ponemos el logging en false no me muestra todo el sql de sequelize
 
 module.exports = { sequelize, DataTypes, Op }
