e0098d3973d00d5c8c6a3ce9e88ff702db57514a
martin <barreiromartinj@gmail.com>
2022-07-13 19:11:46 -0300

    order

--- db/db.js
+++ db/db.js
@@ -1,23 +1,23 @@
 const { Sequelize, DataTypes, Op } = require('sequelize');
-const sequelize = new Sequelize({
-    database: "d1olon79irc05k",
-    username: "xhnhxkqzilphew",
-    password: "cfa5bb8911032d2c4f4061fe5f806edb9d315112f34cb40fc472b859bdc102ad",
-    host: "ec2-52-86-115-245.compute-1.amazonaws.com",
-    port: 5432,
-    dialect: "postgres",
-    logging: false ,
-    dialectOptions: {
-      ssl: {
-        require: true, // This will help you. But you will see nwe error
-        rejectUnauthorized: false // This line will fix new error
-      },
-      logging: false,
-      native: false,
-    },
-  },);
+// const sequelize = new Sequelize({
+//     database: "d1olon79irc05k",
+//     username: "xhnhxkqzilphew",
+//     password: "cfa5bb8911032d2c4f4061fe5f806edb9d315112f34cb40fc472b859bdc102ad",
+//     host: "ec2-52-86-115-245.compute-1.amazonaws.com",
+//     port: 5432,
+//     dialect: "postgres",
+//     logging: false ,
+//     dialectOptions: {
+//       ssl: {
+//         require: true, // This will help you. But you will see nwe error
+//         rejectUnauthorized: false // This line will fix new error
+//       },
+//       logging: false,
+//       native: false,
+//     },
+//   },);
 
-  // const sequelize = new Sequelize('postgres://postgres:cr1st1n4buf1d1s@localhost:5432/martinPrueba', { logging: false })
+  const sequelize = new Sequelize('postgres://postgres:cr1st1n4buf1d1s@localhost:5432/martinPrueba', { logging: false })
 
 /////////////////////////////////////// si ponemos el logging en false no me muestra todo el sql de sequelize
 
