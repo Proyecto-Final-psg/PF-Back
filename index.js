70570cdaa4d072daf9a81507e0bc017f479f8843
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-07-14 20:19:21 -0500

    modularizacion rutas orders y funciones

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const category = require('./routes/category');
 const product = require('./routes/product');
 const user = require('./routes/user')
+const order = require('./routes/order')
 const app = express()
 const cors = require("cors")
 app.use(express.json());
@@ -40,96 +41,10 @@ const jwtCheck = jwt.expressjwt({
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.use('/', user)
+app.use('/', order)
 app.use('/', category)
 app.use('/products', product)
 
-//////////////////////////
-
-app.post('/addOrder', async (req, res) => {
-    try {
-        let { user_id, address, status, arrayItems } = req.body
-
-        let user = await User.findOne({ where: { user_id: user_id } })
-
-        var nuevaOrder = await Order.create({
-            "address": address,
-            "status": status
-        })
-
-        nuevaOrder.setUser(user)
-        // nuevaOrder.save()
-
-        for (let i = 0; i < arrayItems.length; i++) {
-
-            let orderItem = await OrderItem.create({
-                quantity: arrayItems[i].quantity,
-                price: arrayItems[i].price
-            })
-            let product = await Product.findOne({ where: { id: arrayItems[i].product_id } })
-
-            orderItem.setProduct(product)
-
-            orderItem.setOrder(nuevaOrder)
-        }
-        res.json("Order cargada correctamente")
-
-    } catch (error) {
-        console.log(error)
-        return res.status(400).send({
-            name: error.name,
-            msg: error.message
-        })
-    }
-})
 
 
-
-app.get('/getOrders/:user_id', async (req, res) => {
-    let { user_id } = req.params
-
-    try {
-        let listaDordenes = []
-        let user = await User.findOne({ where: { user_id: user_id } })
-        let ordenes = await user.getOrders({ include: OrderItem })
-
-        for (let i = 0; i < ordenes.length; i++) {
-
-            let orden = ordenes[i].dataValues
-
-            let ordenUser = {
-                order_id: orden.id,
-                user_id: orden.userUserId,
-                address: orden.address,
-                status: orden.status,
-                arrayItems: []
-            }
-
-            let items = ordenes[i].dataValues.order_items
-
-            for (let j = 0; j < items.length; j++) {
-
-                let product_id = items[j].dataValues.productId
-                let producto = await Product.findOne({ where: { id: product_id } })
-
-              
-
-                producto.dataValues.quantity = items[j].dataValues.quantity
-
-
-                ordenUser.arrayItems.push(producto.dataValues)
-            }
-            listaDordenes.push(ordenUser)
-
-        }
-        res.json(listaDordenes)
-    } catch (error) {
-        console.log(error)
-        return res.status(400).send({
-            name: error.name,
-            msg: error.message
-        })
-    }
-
-
-})
 
---
+++ routes/order.js
@@ -0,0 +1,43 @@
+const {Router} = require('express')
+const {createOrder, getOrders, getAllOrders} = require('../controllers/orders/functionOrders')
+const router = Router();
+
+router.post('/addOrder', async (req, res) => {
+    let { user_id, address, status, arrayItems } = req.body
+    try {
+        await createOrder(user_id, address, status, arrayItems)
+        res.json("Order cargada correctamente")
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({
+            name: error.name,
+            msg: error.message
+        })
+    }
+})
+
+router.get('/getAllOrders', async (req, res) => {
+  try {
+    res.json(await getAllOrders())
+  } catch (error) {
+    res.status(400).send({
+        name: error.name,
+        msg: error.message
+    })
+  }
+})
+
+router.get('/getOrders/:user_id', async (req, res) => {
+    let { user_id } = req.params
+    try {
+        res.json(await getOrders(user_id))
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({
+            name: error.name,
+            msg: error.message
+        })
+    }
+})
+
+module.exports = router
