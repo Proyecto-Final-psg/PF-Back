b539b8e67983013696c87d6cfc0a177a60aab0ff
martin <barreiromartinj@gmail.com>
2022-07-15 11:59:09 -0300

    url mercado pago

--- controllers/orders/functionOrders.js
+++ controllers/orders/functionOrders.js
@@ -6,52 +6,63 @@ const { payOrder } = require('../mercadoPago/mercadoPago')
 
 module.exports = {
     createOrder: async (user_id, address, status, arrayItems) => {
-
-        let urlPago = await payOrder(arrayItems)
         let user = await User.findOne({ where: { user_id: user_id } })
-        console.log(urlPago)
-        var nuevaOrder = await Order.create({
-            "address": address,
-            "status": status,
-            "urlPago": urlPago
-        })
-        nuevaOrder.setUser(user)
-        // nuevaOrder.save()
-        for (let i = 0; i < arrayItems.length; i++) {
-            let orderItem = await OrderItem.create({
-                quantity: arrayItems[i].quantity,
-                price: arrayItems[i].price
+
+        if (user) {
+            let urlPago = await payOrder(arrayItems)
+            console.log(urlPago)
+            var nuevaOrder = await Order.create({
+                "address": address,
+                "status": status,
+                "urlPago": urlPago
             })
-            let product = await Product.findOne({ where: { id: arrayItems[i].product_id } })
-            orderItem.setProduct(product)
-            orderItem.setOrder(nuevaOrder)
+            nuevaOrder.setUser(user)
+            // nuevaOrder.save()
+            for (let i = 0; i < arrayItems.length; i++) {
+                let orderItem = await OrderItem.create({
+                    quantity: arrayItems[i].quantity,
+                    price: arrayItems[i].price
+                })
+                let product = await Product.findOne({ where: { id: arrayItems[i].product_id } })
+                orderItem.setProduct(product)
+                orderItem.setOrder(nuevaOrder)
+            }
+
+            return urlPago
+        } else {
+            return { res: "USER DONT EXIST" }
         }
 
-        return urlPago
+
     },
     getOrders: async (user_id) => {
         let listaDordenes = []
         let user = await User.findOne({ where: { user_id: user_id } })
-        let ordenes = await user.getOrders({ include: OrderItem })
-        for (let i = 0; i < ordenes.length; i++) {
-            let orden = ordenes[i].dataValues
-            let ordenUser = {
-                order_id: orden.id,
-                user_id: orden.userUserId,
-                address: orden.address,
-                status: orden.status,
-                arrayItems: []
+        if (user) {
+            let ordenes = await user.getOrders({ include: OrderItem })
+            for (let i = 0; i < ordenes.length; i++) {
+                let orden = ordenes[i].dataValues
+                let ordenUser = {
+                    order_id: orden.id,
+                    user_id: orden.userUserId,
+                    address: orden.address,
+                    status: orden.status,
+                    arrayItems: []
+                }
+                let items = ordenes[i].dataValues.order_items
+                for (let j = 0; j < items.length; j++) {
+                    let product_id = items[j].dataValues.productId
+                    let producto = await Product.findOne({ where: { id: product_id } })
+                    producto.dataValues.quantity = items[j].dataValues.quantity
+                    ordenUser.arrayItems.push(producto.dataValues)
+                }
+                listaDordenes.push(ordenUser)
             }
-            let items = ordenes[i].dataValues.order_items
-            for (let j = 0; j < items.length; j++) {
-                let product_id = items[j].dataValues.productId
-                let producto = await Product.findOne({ where: { id: product_id } })
-                producto.dataValues.quantity = items[j].dataValues.quantity
-                ordenUser.arrayItems.push(producto.dataValues)
-            }
-            listaDordenes.push(ordenUser)
+            return listaDordenes
         }
-        return listaDordenes
+        else {
+            return { res: "USER DONT EXIST" }
+        }
     },
     getAllOrders: async () => {
         return await Order.findAll()
