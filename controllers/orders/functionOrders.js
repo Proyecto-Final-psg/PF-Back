8ee46781c08430843d0317af74188a29f055a1a3
Juano <juanocataldo@gmail.com>
2022-07-22 09:33:07 -0300

    J: modifiqué un parámetro para que tome user_email en vez de username, ya que cuando era guest tiraba null en username

--- controllers/orders/functionOrders.js
+++ controllers/orders/functionOrders.js
@@ -137,10 +137,11 @@ module.exports = {
         for (let i = 0; i < order.length; i++) {
             let p = {
                 order_id: order[i].dataValues.id,
-                username: order[i].dataValues.userUserId,
+                username: order[i].dataValues.user_email,
                 total: 0
             }
             let order_items = order[i].dataValues.order_items
+            console.log(order_items);
             let total = 0
             for (let j = 0; j < order_items.length; j++) {
                 let price = order_items[j].dataValues.price
