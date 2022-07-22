17daf7dc6062e4ef986300f0fbd804bf96bc5a86
Juano <juanocataldo@gmail.com>
2022-07-22 09:54:08 -0300

    J: traigo User completo embebido

--- controllers/orders/functionOrders.js
+++ controllers/orders/functionOrders.js
@@ -132,11 +132,12 @@ module.exports = {
         return order
     },
     getTotalByUserByOrder: async () => {
-        const order = await Order.findAll({ include: OrderItem })
+        const order = await Order.findAll({ include: OrderItem, User })
         const product = []
         for (let i = 0; i < order.length; i++) {
             let p = {
                 order_id: order[i].dataValues.id,
+                user: await User.findByPk(order[i].dataValues.userUserId) ,
                 username: order[i].dataValues.user_email,
                 total: 0
             }
