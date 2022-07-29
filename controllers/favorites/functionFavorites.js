912f48b65feefb64d2cb540070b64e0f22f7bb83
Florencia Taburelli <flor.t.c@gmail.com>
2022-07-29 15:35:23 -0400

    fix delete favourite

--- controllers/favorites/functionFavorites.js
+++ controllers/favorites/functionFavorites.js
@@ -37,9 +37,8 @@ module.exports = {
         return favoriteAdded
     },
     removeFavorite : async(id) =>{
-        const item = await favorites.findByPk(id, {includes : ['product']})
-        await favorites.destroy({where : {id : id}})
-        return `The product ${item.dataValues.product} has been removed from your wishlist`
+        await favorites.destroy({where : {'product' : id}})
+        return `The product  has been removed from your wishlist`
     },
     ///  funcion prueba
     myfunct : async() =>{
